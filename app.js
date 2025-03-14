bcdc4bd6317477bb3e877363c31b9b423dbe828d
vrendra Sah <rajvirendra8875@gmail.com>
2025-03-15 04:33:02 +0530

    2day of working

--- app.js
+++ app.js
@@ -11,3 +11,16 @@ console.log(xicon)
 xicon.addEventListener("click",()=>{
     menuBlackbg.classList.remove("ShowmenuBlackBg")
 })
+
+let TabMenuIcon=document.querySelector(".tabMenuIcon")
+let TabMenuItems=document.querySelector(".TabmenuItems")
+TabMenuIcon.addEventListener("click",()=>{
+    if(TabMenuIcon.innerHTML.includes('<i class="fa-solid fa-bars"></i>')){
+        TabMenuIcon.innerHTML='<i class="fa-solid fa-xmark"></i>'
+        TabMenuItems.style.display="block";
+    }
+    else{
+        TabMenuIcon.innerHTML='<i class="fa-solid fa-bars"></i>'
+        TabMenuItems.style.removeProperty("display")
+    }
+})
